ce5b361177f408e5eef92eff491688bc9393e914
dalex1981a <168557868+dalex1981a@users.noreply.github.com>
2024-06-01 00:03:47 +0000

    completed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,56 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomIndex(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+
+function randomItem(arr) {
+  const index = randomIndex(arr);
+  return arr[index];
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Excuse generator console");
+
+  const whos = [
+    "The dog",
+    "My Teacher",
+    "My boyfriend",
+    "My mom",
+    "My teacher",
+    "My boss"
+  ];
+  const whats = [
+    "ate",
+    "pooped",
+    "threw away",
+    "destroyed",
+    "burned",
+    "lost",
+    "sold"
+  ];
+  const avoids = [
+    "my homework",
+    "my tickets",
+    "my bills",
+    "my recipes",
+    "my projects"
+  ];
+  const whens = [
+    "last month!",
+    "yesterday!",
+    "last week!",
+    "last year!",
+    "last birthday!"
+  ];
+
+  const who = randomItem(whos);
+  const what = randomItem(whats);
+  const avoid = randomItem(avoids);
+  const when = randomItem(whens);
+
+  const randomExcuse = who + " " + what + " " + avoid + " " + when;
+  document.querySelector("h1").innerText = randomExcuse;
 };
